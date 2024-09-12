42b67fbef4b0c830fa56db22a6e5d006735ae998
divya <dslidder@iastate.edu>
2024-09-12 02:44:16 -0500

    tested login form

--- Experiments/dslidder/main.py
+++ Experiments/dslidder/main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from typing import Annotated
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-    
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+@app.post("/login/")
+async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    return {"username": username}
