7b27c296ea0e261d8efbf03445d34d2c5d463930
alexleyoung <139512340+alexleyoung@users.noreply.github.com>
2024-09-30 12:53:34 -0500

    fix responsiveness of login/signin page

--- Frontend/asap-web/src/app/(signin)/page.tsx
+++ Frontend/asap-web/src/app/(signin)/page.tsx
@@ -10,40 +10,37 @@ export default function Home() {
   const toggleForm = () => {
     setIsSignIn(!isSignIn);
   };
+
   return (
-    <main className="w-full lg:grid lg:grid-cols-2 h-screen">
-      <div>
-        {isSignIn ? (
-          <>
-            <SignInForm />
-            <div className="-mt-24 text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <Link href="#" className="underline" onClick={toggleForm}>
-                Sign Up
-              </Link>
-            </div>{" "}
-          </>
-        ) : (
-          <>
-            <SignUpForm />
-            <div className="-mt-24 text-center text-sm">
-              Already have an account?{" "}
-              <Link href="#" className="underline" onClick={toggleForm}>
-                Sign In
-              </Link>
-            </div>
-          </>
-        )}
-      </div>
-      <div>
-        <Image
-          src="/images/signin.svg"
-          alt="image here"
-          width={500}
-          height={500}
-          layout="responsive"
-        />
-      </div>
+    <main className='w-full lg:grid lg:grid-cols-2 h-screen'>
+      {isSignIn ? (
+        <div>
+          <SignInForm />
+          <div className='-mt-24 text-center text-sm'>
+            Don&apos;t have an account?{" "}
+            <Link href='#' className='underline' onClick={toggleForm}>
+              Sign Up
+            </Link>
+          </div>{" "}
+        </div>
+      ) : (
+        <div>
+          <SignUpForm />
+          <div className='-mt-24 text-center text-sm'>
+            Already have an account?{" "}
+            <Link href='#' className='underline' onClick={toggleForm}>
+              Sign In
+            </Link>
+          </div>
+        </div>
+      )}
+      <Image
+        src='/images/signin.svg'
+        alt='image here'
+        width={500}
+        height={500}
+        className='hidden lg:block'
+      />
     </main>
   );
 }
