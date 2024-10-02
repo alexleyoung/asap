64704ab1949e657f8dc25c2339c1594890cda6da
caitiharts <137228789+caitiharts@users.noreply.github.com>
2024-10-02 16:18:11 -0500

    passwords are now hashed!!

--- Backend/crud.py
+++ Backend/crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-
+import bcrypt
 from Backend import models, schemas
 
 def get_user(db: Session, userID: int):
@@ -12,11 +12,11 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed = user.password + "notHashedLOL"
+    hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
     db_user = models.User(firstName=user.firstName, 
                           lastName=user.lastName, 
                           email=user.email, 
-                          hashedPassword=fake_hashed)
+                          hashedPassword=hashed_password.decode('utf-8'))
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
