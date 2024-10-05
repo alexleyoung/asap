ee90c0ce3bd6eb162a07f80dc5e05a54948bd8d2
kaylee535 <kaymaczek@gmail.com>
2024-10-05 15:53:45 -0500

    added getprotecteddata

--- Frontend/asap-web/src/lib/auth.ts
+++ Frontend/asap-web/src/lib/auth.ts
@@ -3,14 +3,13 @@ export const signUp = async (
   lastName: string,
   email: string,
   password: string
-  
 ) => {
   const response = await fetch("http://localhost:8000/users/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({firstName, lastName, email, password}),
+    body: JSON.stringify({ firstName, lastName, email, password }),
   });
   //   const data = await response.json();
   //   if (!response.ok) {
@@ -31,4 +30,22 @@ export const signIn = async (email: string, password: string) => {
     throw new Error(data.error || "Something went wrong");
   }
   return data;
+};
+
+const getProtectedData = async (endpoint: string) => {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    throw new Error("Unauthorized");
+  }
+  const response = await fetch(`http://localhost:8000/${endpoint}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${endpoint}`,
+      "Content-Type": "application/json",
+    },
+  });
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+  return response.json();
 };
