9cb9edacdd93410a27ce019e06a8e9aef905cee3
alexleyoung <139512340+alexleyoung@users.noreply.github.com>
2024-10-30 13:36:45 -0500

    add jwt to client signup function

--- Frontend/asap-web/src/components/signup/SignUpForm.tsx
+++ Frontend/asap-web/src/components/signup/SignUpForm.tsx
@@ -85,27 +85,26 @@ export default function SignUpForm() {
   };
 
   return (
-    <div className="grid h-full place-items-center py-12 border-r">
-      <div className="mx-auto grid w-[350px] gap-6">
-        <div className="grid gap-2 text-center">
-          <h1 className="text-3xl font-bold">Sign Up</h1>
-          <p className="text-balance text-muted-foreground">
+    <div className='grid h-full place-items-center py-12 border-r'>
+      <div className='mx-auto grid w-[350px] gap-6'>
+        <div className='grid gap-2 text-center'>
+          <h1 className='text-3xl font-bold'>Sign Up</h1>
+          <p className='text-balance text-muted-foreground'>
             Enter your info below to create your account
           </p>
         </div>
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(handleSignUp)}
-            className="space-y-8"
-          >
+            className='space-y-8'>
             <FormField
               control={form.control}
-              name="firstname"
+              name='firstname'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>First Name</FormLabel>
                   <FormControl>
-                    <Input {...field} type="text" placeholder="First Name" />
+                    <Input {...field} type='text' placeholder='First Name' />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -113,12 +112,12 @@ export default function SignUpForm() {
             />
             <FormField
               control={form.control}
-              name="lastname"
+              name='lastname'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Last Name</FormLabel>
                   <FormControl>
-                    <Input {...field} type="text" placeholder="Last Name" />
+                    <Input {...field} type='text' placeholder='Last Name' />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -127,12 +126,12 @@ export default function SignUpForm() {
 
             <FormField
               control={form.control}
-              name="email"
+              name='email'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
-                    <Input {...field} type="email" placeholder="m@email.com" />
+                    <Input {...field} type='email' placeholder='m@email.com' />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
@@ -140,15 +139,15 @@ export default function SignUpForm() {
             />
             <FormField
               control={form.control}
-              name="password"
+              name='password'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Password</FormLabel>
                   <FormControl>
                     <Input
                       {...field}
-                      type="password"
-                      placeholder="create a password"
+                      type='password'
+                      placeholder='create a password'
                     />
                   </FormControl>
                   <FormMessage />
@@ -157,37 +156,37 @@ export default function SignUpForm() {
             />
             <FormField
               control={form.control}
-              name="confirmpassword"
+              name='confirmpassword'
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Confirm Password</FormLabel>
                   <FormControl>
                     <Input
                       {...field}
-                      type="password"
-                      placeholder="confirm password"
+                      type='password'
+                      placeholder='confirm password'
                     />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
-            {error && <p className="text-red-500 text-sm">{error}</p>}
-            {success && <p className="text-green-500 text-sm">{success}</p>}
-            <Button type="submit" className="w-full">
+            {error && <p className='text-red-500 text-sm'>{error}</p>}
+            {success && <p className='text-green-500 text-sm'>{success}</p>}
+            <Button type='submit' className='w-full'>
               Sign Up
             </Button>
           </form>
         </Form>
 
-        <Separator className="relative my-2">
-          <span className="absolute px-2 py-1 bg-background left-[45%] -top-4 text-primary/60">
+        <Separator className='relative my-2'>
+          <span className='absolute px-2 py-1 bg-background left-[45%] -top-4 text-primary/60'>
             or
           </span>
         </Separator>
-        <Button variant="outline" className="w-full flex gap-2">
+        <Button variant='outline' className='w-full flex gap-2'>
           <FcGoogle size={20} />
-          <Link href="/dashboard">Sign Up with Google</Link>
+          <Link href='/dashboard'>Sign Up with Google</Link>
         </Button>
       </div>
     </div>
