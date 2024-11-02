979173bfdb13d05f4a178b60ef3719dfa7cb5b0b
alexleyoung <139512340+alexleyoung@users.noreply.github.com>
2024-11-02 01:19:17 -0500

    error 402 logging

--- Backend/main.py
+++ Backend/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer
 
+# For logging
+import logging
+from fastapi import Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
 from .routers import users, events, tasks, calendars, auth
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -15,6 +21,18 @@ app.include_router(users.router)
 app.include_router(events.router)
 app.include_router(tasks.router)
 app.include_router(calendars.router)
+
+
+# Unprocessable Entity error handler
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    exc_str = f"{exc}".replace("\n", " ").replace("   ", " ")
+    logging.error(f"{request}: {exc_str}")
+    content = {"status_code": 10422, "message": exc_str, "data": None}
+    return JSONResponse(
+        content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY
+    )
+
 
 # allows cross origin resource sharing (stuff is on dif ports)
 app.add_middleware(
