4950f69efb3888bbe0d97e99a92ec92bd3224a45
caitiharts <137228789+caitiharts@users.noreply.github.com>
2024-11-06 21:38:55 -0600

    try try AGAIN

--- Backend/main.py
+++ Backend/main.py
@@ -22,6 +22,7 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(events.router)
+app.include_router(events.newRouter) 
 app.include_router(tasks.router)
 app.include_router(calendars.router)
 
