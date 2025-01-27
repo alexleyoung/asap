b882f02601deed57cc7cd020f8e447d3b5a22270
Alex Young <alexyoung0119@gmail.com>
2025-01-26 20:45:51 -0600

    fix lint errors in landing page

--- Frontend/asap/src/components/ui/text-generate-effect.tsx
+++ Frontend/asap/src/components/ui/text-generate-effect.tsx
@@ -15,7 +15,7 @@ export const TextGenerateEffect = ({
   duration?: number;
 }) => {
   const [scope, animate] = useAnimate();
-  let wordsArray = words.split(" ");
+  const wordsArray = words.split(" ");
   useEffect(() => {
     animate(
       "span",
@@ -37,11 +37,10 @@ export const TextGenerateEffect = ({
           return (
             <motion.span
               key={word + idx}
-              className="dark:text-white text-black opacity-0"
+              className='dark:text-white text-black opacity-0'
               style={{
                 filter: filter ? "blur(10px)" : "none",
-              }}
-            >
+              }}>
               {word}{" "}
             </motion.span>
           );
@@ -52,8 +51,8 @@ export const TextGenerateEffect = ({
 
   return (
     <div className={cn("font-bold", className)}>
-      <div className="mt-4">
-        <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
+      <div className='mt-4'>
+        <div className=' dark:text-white text-black text-2xl leading-snug tracking-wide'>
           {renderWords()}
         </div>
       </div>
